db75bb854bf9b1f7029664dbe40814b04e14ec66
JUSTLOVELE <34121275+JUSTLOVELE@users.noreply.github.com>
2020-04-01 22:56:31 +0800

    add

--- android_case/google/FirstApp/app/src/main/java/com/e/firstapp/MainActivity.java
+++ android_case/google/FirstApp/app/src/main/java/com/e/firstapp/MainActivity.java
@@ -1,14 +1,20 @@
-package com.cornell_01;
+package com.e.firstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
+
+    private EditText editText;
+
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +24,17 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                button.setText("hello");
+                sendMessage(v);
             }
         });
+    }
+
+    private void sendMessage(View view) {
+
+        editText = findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
