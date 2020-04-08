8f549d4d0e043a75cc3ecd777267869fc4220a14
JUSTLOVELE <34121275+JUSTLOVELE@users.noreply.github.com>
2020-04-08 19:40:31 +0800

    add

---
+++ swift_case/cornell/Cornell_03/Cornell_03/ModalViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ModalViewController.swift
+//  Cornell_03
+//
+//  Created by 杨祖亮 on 2020/4/8.
+//  Copyright © 2020 杨祖亮. All rights reserved.
+//
+
+import UIKit
+
+class ModalViewController: UIViewController {
+    
+    var button: UIButton!
+    var textField: UITextField!
+    
+    weak var delegate: SaveNewNameProtocol?
+    
+    init(delegate: SaveNewNameProtocol?) {
+        super.init(nibName: nil, bundle: nil)
+        self.delegate = delegate
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        
+        textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        //textField.text = titleString
+        textField.borderStyle = .roundedRect
+        textField.backgroundColor = .white
+        textField.textAlignment = .center
+        textField.clearsOnBeginEditing = true
+        view.addSubview(textField)
+        
+        button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Dismiss and save text", for: .normal)
+        button.backgroundColor = .white
+        button.setTitleColor(.red, for: .normal)
+        
+        // When the button is pressed, dismiss this ModalViewController and change the button name
+        button.addTarget(self, action: #selector(dismissViewControllerAndSaveText), for: .touchUpInside)
+        view.addSubview(button)
+        
+        setupConstraints()
+    }
+    
+    func setupConstraints() {
+        // textField constraints
+        NSLayoutConstraint.activate([
+            textField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 24),
+            textField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            textField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            textField.heightAnchor.constraint(equalToConstant: 24)
+        ])
+        
+        // button constraints
+        NSLayoutConstraint.activate([
+            button.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 16),
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.heightAnchor.constraint(equalToConstant: 48)
+        ])
+    }
+    
+    @objc func dismissViewControllerAndSaveText() {
+        
+        
+        delegate?.saveNewName(newName: textField.text!)
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
--- swift_case/cornell/Cornell_03/Cornell_03/ViewController.swift
+++ swift_case/cornell/Cornell_03/Cornell_03/ViewController.swift
@@ -8,13 +8,66 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+protocol SaveNewNameProtocol: class {
+    
+    func saveNewName(newName: String)
 }
 
+class ViewController: UIViewController {
+    
+    var pushNavViewControllerButton: UIButton!
+    var presentModalViewControllerButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        // Set the navigation controller's title!
+        
+        pushNavViewControllerButton = UIButton()
+        pushNavViewControllerButton.translatesAutoresizingMaskIntoConstraints = false
+        pushNavViewControllerButton.setTitle("Push Navigation VC", for: .normal)
+        pushNavViewControllerButton.setTitleColor(.blue, for: .normal)
+        pushNavViewControllerButton.addTarget(self, action: #selector(pushNavViewController), for: .touchUpInside)
+        view.addSubview(pushNavViewControllerButton)
+        
+        presentModalViewControllerButton = UIButton()
+        presentModalViewControllerButton.translatesAutoresizingMaskIntoConstraints = false
+        presentModalViewControllerButton.setTitle("Present Modal VC [change my name]", for: .normal)
+        presentModalViewControllerButton.setTitleColor(.red, for: .normal)
+        presentModalViewControllerButton.addTarget(self, action: #selector(presentModalViewController), for: .touchUpInside)
+        view.addSubview(presentModalViewControllerButton)
+        
+        setupConstraints()
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            pushNavViewControllerButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            pushNavViewControllerButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            pushNavViewControllerButton.heightAnchor.constraint(equalToConstant: 24)
+        ])
+        
+        NSLayoutConstraint.activate([
+            presentModalViewControllerButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            presentModalViewControllerButton.topAnchor.constraint(equalTo: pushNavViewControllerButton.bottomAnchor, constant: 16),
+            presentModalViewControllerButton.heightAnchor.constraint(equalToConstant: 24)
+        ])
+    }
+    
+    @objc func pushNavViewController() {
+    }
+    
+    @objc func presentModalViewController() {
+        
+        let vc = ModalViewController(delegate: self)
+        self.present(vc, animated: true, completion: nil)
+    }
+}
+
+extension ViewController: SaveNewNameProtocol {
+    
+    func saveNewName(newName: String) {
+        presentModalViewControllerButton.setTitle(newName, for: .normal)
+    }
+}
