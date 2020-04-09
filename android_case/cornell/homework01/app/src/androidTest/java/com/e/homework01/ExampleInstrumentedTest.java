467d6457123b359a18a1f9954a54bb9bc4044569
JUSTLOVELE <34121275+JUSTLOVELE@users.noreply.github.com>
2020-04-09 22:49:47 +0800

    update

--- android_case/cornell/homework01/app/src/androidTest/java/com/e/homework01/ExampleInstrumentedTest.java
+++ android_case/cornell/homework01/app/src/androidTest/java/com/e/homework01/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.e.homework01;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
--- android_case/cornell/homework01/app/src/main/java/com/e/homework01/MainActivity.java
+++ android_case/cornell/homework01/app/src/main/java/com/e/homework01/MainActivity.java
@@ -1,6 +1,7 @@
 package com.e.homework01;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
