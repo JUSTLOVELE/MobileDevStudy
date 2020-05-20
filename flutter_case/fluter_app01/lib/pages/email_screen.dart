499af21405fc64d204f9a79b5640587c278c909d
JUSTLOVELE <34121275+JUSTLOVELE@users.noreply.github.com>
2020-05-20 18:00:41 +0800

    add

--- flutter_case/fluter_app01/lib/pages/email_screen.dart
+++ flutter_case/fluter_app01/lib/pages/email_screen.dart
@@ -1,11 +1,57 @@
 import 'package:flutter/material.dart';
+import 'custome_route.dart';
 
 class EmailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('email')),
-      body: Center(child: Text('email')),
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('FirstPage', style: TextStyle(fontSize: 36.0)),
+        elevation: 0.0,
+      ),
+      body: Center(
+          child: MaterialButton(
+        child: Icon(
+          Icons.navigate_next,
+          color: Colors.white,
+          size: 64.0,
+        ),
+        onPressed: () {
+
+          // Navigator.of(context)
+          //      // .push(CustomRoute(SecondPage));
+          //     .push(MaterialPageRoute(builder: (BuildContext context) {
+          //   return SecondPage();
+          // }
+          // ));
+          Navigator.of(context).push(CustomRoute(SecondPage()));
+        },
+      )),
+    );
+  }
+}
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.pinkAccent,
+      appBar: AppBar(
+        title: Text('SecondPage',style:TextStyle(fontSize:36.0),),
+        backgroundColor: Colors.pinkAccent,
+        leading:Container(),
+        elevation: 0.0,
+      ),
+      body:Center(
+        child: MaterialButton(
+          child: Icon(
+            Icons.navigate_before,
+            color:Colors.white,
+            size:64.0
+          ),
+          onPressed: ()=>Navigator.of(context).pop(),
+        ),
+      )
     );
   }
 }
