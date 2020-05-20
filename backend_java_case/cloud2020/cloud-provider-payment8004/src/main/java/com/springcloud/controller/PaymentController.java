37e8f815bca897046acab23aa743ed2469b09e74
JUSTLOVELE <694105388@qq.com>
2020-05-20 15:59:51 +0800

    add

---
+++ backend_java_case/cloud2020/cloud-provider-payment8004/src/main/java/com/springcloud/controller/PaymentController.java
@@ -0,0 +1,20 @@
+package com.springcloud.controller;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PaymentController {
+
+	@Value("${server.port}")
+	private String serverPort;
+	
+	@RequestMapping(value = "/payment/zk")
+	public String paymentzk() {
+		return "Springcloud with zookeeper: " + serverPort + "\t" + UUID.randomUUID().toString();
+	}
+	
+}
