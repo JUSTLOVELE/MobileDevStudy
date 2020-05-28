4b541a1600ed962c8ce34d46e011e7bc881247be
JUSTLOVELE <34121275+JUSTLOVELE@users.noreply.github.com>
2020-05-28 16:53:35 +0800

    add

---
+++ flutter_case/flutter_app/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'index/index.dart';
+
+void main()=> runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "知乎-高仿版",
+      home: Index(),
+    );
+  }
+}
