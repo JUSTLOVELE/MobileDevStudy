9f20719d05a30aadbede6a8c92bb6be28ef1484e
JUSTLOVELE <34121275+JUSTLOVELE@users.noreply.github.com>
2020-05-28 15:02:10 +0800

    add

--- android_case/google/FirstApp/app/src/main/java/com/e/firstapp/MainActivity.java
+++ android_case/google/FirstApp/app/src/main/java/com/e/firstapp/MainActivity.java
@@ -2,39 +2,112 @@ package com.e.firstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.android.material.tabs.TabLayout;
+
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import me.majiajie.pagerbottomtabstrip.PageNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private List<Fragment> mFragmensts = new ArrayList<>();
+    private MyViewPagerAdapter adapter;
+    private static final String TAG = "MainActivity";
 
-    private Button button;
+    @BindView(R.id.tab_layout)
+    TabLayout mTabLayout;
 
-    private EditText editText;
-
-    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    @BindView(R.id.vp_container)
+    ViewPager view_pager;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendMessage(v);
-            }
-        });
+        initView();
+
     }
 
-    private void sendMessage(View view) {
+    private void initView() {
+        mTabLayout = findViewById(R.id.tab_layout);
+        view_pager = findViewById(R.id.vp_container);
+//        mFragmensts = DataGenerator.getFragments("TabLayout Tab");
+        mFragmensts.add(new HomeFragment());
+        mFragmensts.add(new HomeFragment());
+        mFragmensts.add(new HomeFragment());
+     //   mFragmensts.add(new StudyFragment());
+     //   mFragmensts.add(new MeFragment());
+        adapter = new MyViewPagerAdapter(getSupportFragmentManager(),mFragmensts);
+        view_pager.setAdapter(adapter);
+        view_pager.setOffscreenPageLimit(2);
+        view_pager.setCurrentItem(0,false);
+        for(int i=0;i<3;i++){
+            mTabLayout.addTab(mTabLayout.newTab().setCustomView(DataGenerator.getTabView(this,i)));
+        }
+        chooseFirst();
+        mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Log.e(TAG, "onTabSelected: " + tab.getPosition());
+                view_pager.setCurrentItem(tab.getPosition(),true);
+                recoverItem();
+                View view =tab.getCustomView();
+                ImageView imageView = view.findViewById(R.id.tab_content_image);
+                TextView textView = view.findViewById(R.id.tab_content_text);
+                imageView.setImageDrawable(getResources().getDrawable(DataGenerator.mTabResPressed[tab.getPosition()]));
+                textView.setTextColor(getResources().getColor(R.color.colorAccent));
+            }
 
-        editText = findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        view_pager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
+    }
+
+    /**
+     * 初始化
+     */
+    private void chooseFirst(){
+        TabLayout.Tab tabAt =  mTabLayout.getTabAt(0);
+        View view =tabAt.getCustomView();
+        ImageView imageView = view.findViewById(R.id.tab_content_image);
+        TextView textView = view.findViewById(R.id.tab_content_text);
+        imageView.setImageDrawable(getResources().getDrawable(DataGenerator.mTabResPressed[0]));
+        textView.setTextColor(getResources().getColor(R.color.colorAccent));
+    }
+
+    /**
+     * 恢复默认
+     */
+    private void recoverItem() {
+        for (int i = 0; i < 3; i++) {
+            TabLayout.Tab tabAt =  mTabLayout.getTabAt(i);
+            View view =tabAt.getCustomView();
+            ImageView imageView = view.findViewById(R.id.tab_content_image);
+            TextView textView = view.findViewById(R.id.tab_content_text);
+            imageView.setImageDrawable(getResources().getDrawable(DataGenerator.mTabRes[i]));
+            textView.setTextColor(Color.GRAY);
+        }
     }
 }
