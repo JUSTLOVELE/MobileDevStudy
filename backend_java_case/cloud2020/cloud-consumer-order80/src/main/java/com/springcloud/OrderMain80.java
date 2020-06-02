b67200626da962bf052066e871a4fac2f2646aa6
JUSTLOVELE <694105388@qq.com>
2020-06-02 16:39:44 +0800

    add

--- backend_java_case/cloud2020/cloud-consumer-order80/src/main/java/com/springcloud/OrderMain80.java
+++ backend_java_case/cloud2020/cloud-consumer-order80/src/main/java/com/springcloud/OrderMain80.java
@@ -3,9 +3,12 @@ package com.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+//CLOUD-PROVIDER-PAYMENT这个服务要用随机算法来调用
+@RibbonClient(name = "CLOUD-PROVIDER-PAYMENT", configuration = MyRule.class)
 public class OrderMain80 {
 
 	public static void main(String[] args) {
