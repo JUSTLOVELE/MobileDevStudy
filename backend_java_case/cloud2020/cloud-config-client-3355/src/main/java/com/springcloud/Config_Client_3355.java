00b074bd74d9cd6d7516b9f9d42d58de2c8847fe
JUSTLOVELE <694105388@qq.com>
2020-06-30 15:03:40 +0800

    add

--- backend_java_case/cloud2020/cloud-config-client-3355/src/main/java/com/springcloud/Config_Client_3355.java
+++ backend_java_case/cloud2020/cloud-config-client-3355/src/main/java/com/springcloud/Config_Client_3355.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * 如果github的配置改变了,不重启这个3355的服务配置是不会更新的,必须要手动发送一个Post请求才会更新
+ * curl -X POST "http://localhost:3355/actuator/refresh"
+ *
+ */
 @SpringBootApplication
 @EnableEurekaClient
 public class Config_Client_3355 {
