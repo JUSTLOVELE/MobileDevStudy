d1edbac82b33ab47df3bb2d4be722573200c55f5
JUSTLOVELE <694105388@qq.com>
2020-09-21 14:53:23 +0800

    add

---
+++ backend_java_case/DroolsProject/DroolsCase01/src/main/java/com/entity/ComparisonOperatorEntity.java
@@ -0,0 +1,33 @@
+package com.entity;
+
+import java.util.List;
+
+/**
+ * @author yangzl 2020.09.18
+ * @version 1.00.00
+ * @Description:
+ * @history:
+ */
+public class ComparisonOperatorEntity {
+
+    private String names;
+
+    private List<String> list;
+
+
+    public String getNames() {
+        return names;
+    }
+
+    public void setNames(String names) {
+        this.names = names;
+    }
+
+    public List<String> getList() {
+        return list;
+    }
+
+    public void setList(List<String> list) {
+        this.list = list;
+    }
+}
---
+++ backend_java_case/DroolsProject/DroolsCase01/src/test/java/DroolsTest.java
@@ -0,0 +1,73 @@
+import com.entity.ComparisonOperatorEntity;
+import com.entity.Order;
+import org.drools.core.base.RuleNameEqualsAgendaFilter;
+import org.junit.Test;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+/**
+ * @author yangzl 2020.09.15
+ * @version 1.00.00
+ * @Description:
+ * @history:
+ */
+public class DroolsTest {
+
+
+    /**
+     * 指定规则名称
+     */
+    @Test
+    public void case03() {
+
+        KieServices kieServices = KieServices.Factory.get();
+        //获取Kie容器对象
+        KieContainer kieContainer = kieServices.newKieClasspathContainer();
+        //从Kie容器对象中获取会话对象
+        KieSession session = kieContainer.newKieSession();
+        //Fact对象
+        ComparisonOperatorEntity fact = new ComparisonOperatorEntity();
+        fact.setNames("张三");
+        session.insert(fact);
+        //激活规则,指定规则名称
+        session.fireAllRules(new RuleNameEqualsAgendaFilter("book_discount_1"));
+        session.dispose();
+    }
+
+    @Test
+    public void case02() {
+
+        KieServices kieServices = KieServices.Factory.get();
+        //获取Kie容器对象
+        KieContainer kieContainer = kieServices.newKieClasspathContainer();
+        //从Kie容器对象中获取会话对象
+        KieSession session = kieContainer.newKieSession();
+        //Fact对象
+        ComparisonOperatorEntity fact = new ComparisonOperatorEntity();
+        fact.setNames("张三");
+        session.insert(fact);
+        //激活规则,由Drools框架自动进行规则匹配,如果匹配成功,则执行当前规则
+        session.fireAllRules();
+        session.dispose();
+    }
+
+    @Test
+    public void case01() {
+
+        KieServices kieServices = KieServices.Factory.get();
+        //获取Kie容器对象
+        KieContainer kieContainer = kieServices.newKieClasspathContainer();
+        //从Kie容器对象中获取会话对象
+        KieSession session = kieContainer.newKieSession();
+        //Fact对象
+        Order order = new Order();
+        order.setOriginalPrice(150d);
+        session.insert(order);
+        //激活规则,由Drools框架自动进行规则匹配,如果匹配成功,则执行当前规则
+        session.fireAllRules();
+        session.dispose();
+        //优惠后的价格
+        System.out.println(order.getRealPrice());
+    }
+}
