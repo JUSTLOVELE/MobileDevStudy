409a7608a78f32072b22a8c9e3c57ec1d72dfe4d
JUSTLOVELE <694105388@qq.com>
2020-12-21 15:30:52 +0800

    Update LogAspects.java

--- backend_java_case/JavaFrame/SpringAnnocationFrame/src/main/java/com/study/aop/LogAspects.java
+++ backend_java_case/JavaFrame/SpringAnnocationFrame/src/main/java/com/study/aop/LogAspects.java
@@ -5,46 +5,35 @@ import org.aspectj.lang.annotation.*;
 
 import java.util.Arrays;
 
-/**
- * ������
- * @author Administrator
- *
- */
+
 @Aspect
 public class LogAspects {
-	
-	/**
-	 * ��ȡ�������е���ʽ
-	 * 1.��������
-	 * 2.��������������
-	 */
+
 	@Pointcut("execution(public int com.study.aop.MathCalculator.*(..))")
 	public void pointCut() {
 		
 	}
 
-	//@Before("public int com.study.aop.MathCalculator(int, int)")\
-	//joinPointҪ���ڲ����б�ĵ�һλ
 	@Before("pointCut()")
 	public void logStart(JoinPoint joinPoint) {
 		
 		Object[] args = joinPoint.getArgs();
 		System.out.println("" + joinPoint.getSignature().getName());
-		System.out.println("���п�ʼ...������:" + Arrays.toString(args));
+		System.out.println("logStart:" + Arrays.toString(args));
 	}
 	
 	@After("pointCut()")
 	public void logEnd() {
-		System.out.println("���н���...������:");
+		System.out.println("logEnd");
 	}
 	
 	@AfterReturning(value="pointCut()", returning="result")
 	public void logReturn() {
-		System.out.println("���з���...������:");
+		System.out.println("pointCut:");
 	}
 	
 	@AfterThrowing(value="pointCut()", throwing="exception")
 	public void logException(JoinPoint joinPoint, Exception exception) {
-		System.out.println(joinPoint.getSignature() + "�����쳣...������:" + exception);
+		System.out.println(joinPoint.getSignature() + "logException:" + exception);
 	}
 }
