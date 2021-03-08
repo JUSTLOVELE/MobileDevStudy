b4de6a657beb854ce2e25c4832d9c1935bd1b72b
JUSTLOVELE <694105388@qq.com>
2021-03-08 17:31:46 +0800

    Create WindowTest3_EventWindow.java

---
+++ backend_java_case/BigData/FlinkTutorial/src/main/java/com/apitest/window/WindowTest3_EventWindow.java
@@ -0,0 +1,33 @@
+package com.apitest.window;
+
+import com.apitest.source.com.apitest.beans.SensorReading;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * @author yangzl 2021.03.08
+ * @version 1.00.00
+ * @Description:
+ * @Copyright: Copyright (c) 2017 HYKJ All Rights Reserved
+ * @Company: 福建互医科技有限公司
+ * @history:
+ */
+public class WindowTest3_EventWindow {
+
+    public static void main(String[] args) throws Exception{
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);//设置时间语义
+        //nc -lk 7777
+        DataStreamSource<String> inputstream = env.socketTextStream("localhost", 7777);
+        DataStream<SensorReading> dataStream = inputstream.map(line -> {
+            String[] fields = line.split(",");
+            return new SensorReading(fields[0], new Long(fields[1]), new Double(fields[2]));
+        });
+
+        env.execute();
+    }
+}
