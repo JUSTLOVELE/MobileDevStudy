35e8c6706205a62e70e4579d21930cc81b9857ee
JUSTLOVELE <34121275+JUSTLOVELE@users.noreply.github.com>
2021-03-27 00:01:59 +0800

    Update Case04_ComletionStage.java

--- backend_java_case/VERT.X/case01_vertX_web/src/main/java/com/ex/core/study_case/Case04_ComletionStage.java
+++ backend_java_case/VERT.X/case01_vertX_web/src/main/java/com/ex/core/study_case/Case04_ComletionStage.java
@@ -11,11 +11,15 @@ public class Case04_ComletionStage {
     public static void main(String[] args) {
 
         Vertx vertx = VertxInitUtil.initVertxEnv();
-        Future<String> future = vertx.createDnsClient().lookup("vertx.io");
+        //Future<String> future = vertx.createDnsClient().lookup("vertx.io");
+        Future<String> future = Future.future(f->{
+            System.out.println("f1");
+            f.complete("f1 result");
+        });
         CompletionStage<String> completionStage = future.toCompletionStage().whenComplete((ip, err) -> {
             if (err != null) {
                 System.out.println("Could not resolve vertx.io");
-                //err.printStackTrace();
+                err.printStackTrace();
             } else {
                 System.out.println("vertx.io => " + ip);
             }
@@ -25,9 +29,8 @@ public class Case04_ComletionStage {
                 .flatMap(str->{
                     String key = UUID.randomUUID().toString();
                     System.out.println(key);
-                    Future<Object> objectFuture = Future.succeededFuture();
                     //return storeInDb(key, str)
-                    return objectFuture;
+                    return future;
                 })
                 .onSuccess(str->{
                     System.out.println("We have a result: " + str);
