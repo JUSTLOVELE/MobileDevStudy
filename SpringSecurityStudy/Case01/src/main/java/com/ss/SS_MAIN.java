25da8ec9c47f12ee8dff4e9d4cd3c2f65f7914f1
JUSTLOVELE <694105388@qq.com>
2021-03-29 10:57:43 +0800

    add

---
+++ SpringSecurityStudy/Case01/src/main/java/com/ss/SS_MAIN.java
@@ -0,0 +1,17 @@
+package com.ss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yangzl 2021.03.29
+ * @version 1.00.00
+ * @Description:
+ * @history:
+ */
+@SpringBootApplication
+public class SS_MAIN {
+    public static void main(String[] args) {
+        SpringApplication.run(SS_MAIN.class);
+    }
+}
---
+++ SpringSecurityStudy/Case01/src/main/java/com/ss/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.ss.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yangzl 2021.03.29
+ * @version 1.00.00
+ * @Description:
+ * @history:
+ */
+@RestController
+public class TestController {
+
+    @GetMapping(value = "/test/add", produces = "application/json; charset=utf-8")
+    public String add() {
+        return "hello security";
+    }
+}
