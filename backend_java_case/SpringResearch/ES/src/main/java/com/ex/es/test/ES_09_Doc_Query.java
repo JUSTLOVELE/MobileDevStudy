4cb08103e7d1b2d6766daf9198aced10076db824
JUSTLOVELE <694105388@qq.com>
2021-05-11 12:53:28 +0800

    Create ES_09_Doc_Query.java

---
+++ backend_java_case/SpringResearch/ES/src/main/java/com/ex/es/test/ES_09_Doc_Query.java
@@ -0,0 +1,48 @@
+package com.ex.es.test;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+
+import java.io.IOException;
+
+/**
+ * @author yangzl 2021.05.11
+ * @version 1.00.00
+ * @Description:
+ * @Copyright: Copyright (c) 2017 HYKJ All Rights Reserved
+ * @Company: 福建互医科技有限公司
+ * @history:
+ */
+public class ES_09_Doc_Query {
+
+    public static void main(String[] args) throws IOException {
+
+        RestHighLevelClient esClient = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http")));
+        SearchRequest request = new SearchRequest();
+        request.source(new SearchSourceBuilder().query(QueryBuilders.matchAllQuery()));
+        SearchResponse response = esClient.search(request, RequestOptions.DEFAULT);
+        SearchHits hits = response.getHits();
+        System.out.println(hits.getTotalHits());
+        System.out.println(response.getTook());
+
+        for(SearchHit hit: hits) {
+            System.out.println(hit.getSourceAsString());
+        }
+
+
+
+        esClient.close();;
+    }
+}
