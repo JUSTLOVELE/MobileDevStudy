a591a4554921ba6b2887553a26ca379b22f91503
JUSTLOVELE <34121275+JUSTLOVELE@users.noreply.github.com>
2021-05-23 19:24:02 +0800

    add

---
+++ flutter_case/myapp/lib/navigation/notice/notice_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+class NoticePage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => new _NoticePageState();
+}
+class _NoticePageState extends State<NoticePage> {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('通知'),
+          // 后面的省略
+          // ......
+        )
+      ),
+    );
+  }
+
+}
