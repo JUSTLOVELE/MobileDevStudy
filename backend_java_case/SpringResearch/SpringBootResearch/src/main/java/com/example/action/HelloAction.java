4138504bfa3028106e0284ea012b941a23725719
JUSTLOVELE <34121275+JUSTLOVELE@users.noreply.github.com>
2021-06-07 00:07:52 +0800

    add

--- backend_java_case/SpringResearch/SpringBootResearch/src/main/java/com/example/action/HelloAction.java
+++ backend_java_case/SpringResearch/SpringBootResearch/src/main/java/com/example/action/HelloAction.java
@@ -1,6 +1,7 @@
 package com.example.action;
 
 import com.example.model.FormModel;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
---
+++ backend_java_case/SpringResearch/SpringBootResearch/src/main/java/com/example/action/NacosConfig.java
@@ -0,0 +1,24 @@
+package com.example.action;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class NacosConfig {
+
+    @Value("${world:}")
+    private String myname;
+
+    /**
+     * 读取nacos配置文件信息
+     * @return
+     */
+    @GetMapping("/nacosConfigTest")
+    public String nacosConfigTest() {
+        System.out.println("myname=" + myname);
+        return this.myname;
+    }
+}
