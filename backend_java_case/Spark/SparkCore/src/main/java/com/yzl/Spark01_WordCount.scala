e5e5ef282dcaefbe46b29876dbf522baaf2c6a17
JUSTLOVELE <694105388@qq.com>
2021-07-09 16:58:33 +0800

    add

--- backend_java_case/Spark/SparkCore/src/main/java/com/yzl/Spark01_WordCount.scala
+++ backend_java_case/Spark/SparkCore/src/main/java/com/yzl/Spark01_WordCount.scala
@@ -1,5 +1,6 @@
 package com.yzl
 
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
@@ -12,9 +13,31 @@ object Spark01_WordCount {
 
   def main(args: Array[String]): Unit = {
     //建立和Spark框架的连接
-    val sparConf = new SparkConf().setMaster("local").setAppName("WordCount")
-    val sc = new SparkContext()
+    //scala版本
+    val sparkConf = new SparkConf()
+    sparkConf.setMaster("local")   //本地单线程运行
+    sparkConf.setAppName("testJob")
+    val sc = new SparkContext(sparkConf)
     //执行业务操作
+    //1、读取文件，获取一行一行的数据
+    // hello world
+    val lines: RDD[String] = sc.textFile("C:\\develop\\github\\MobileDevStudy\\backend_java_case\\Spark\\SparkCore\\src\\main\\resources")
+    //2、将一行数据进行拆分，形成一个一个的单词
+    // hello world => hello, world
+    var words: RDD[String] = lines.flatMap(_.split(" "))//空格隔开分词
+    //3、将数据根据单词进行分组，便于统计
+    // (hello, hello), (world, world)
+    var wordGroup: RDD[(String, Iterable[String])] = words.groupBy(word => word)
+    //4、对分组后的数据进行转换
+    // (hello, 2), (world, 2)
+    val wordToCount = wordGroup.map {
+      case (word, list) => {
+        (word, list.size)
+      }
+    }
+
+    var arrays: Array[(String, Int)] = wordToCount.collect()
+    arrays.foreach(println)
 
     //关闭连接
     sc.stop()
